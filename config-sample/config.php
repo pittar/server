d803cc9b1983a7bed58c2e351c4817f3336061d5
Andrew Pitt <apitt@redhat.com>
2019-09-16 16:45:58 -0400

    Updated config.

--- config-sample/config.php
+++ config-sample/config.php
@@ -1,4 +1,12 @@
 <?php
 $CONFIG = array (
   'check_data_directory_permissions' => false,
+  'datadirectory' => '/opt/app-root/src/data',
+  'dbtype' => 'pgsql',
+  'dbname' => getenv("POSTGRES_DB"),
+  'dbhost' => getenv("POSTGRES_HOST"),
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => getenv("POSTGRES_USER"),
+  'dbpassword' => getenv("POSTGRES_PASSWORD"),
 );
